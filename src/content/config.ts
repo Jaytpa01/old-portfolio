6cf4799ccf89329100ba1ccd3810f585681a284d
Jay <jaytpa01@gmail.com>
2023-09-23 00:06:01 +1000

    Feat: implement a way to allow <Image> to be used

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,20 @@
 import { z, defineCollection } from "astro:content";
 
-const projectSchema = z.object({
-	title: z.string(),
-	description: z.string().optional(),
-	publishDate: z.coerce.date().optional(),
-	tags: z.array(z.string()).optional(),
-	image: z.string().optional(),
-});
+const projectSchema = ({ image }: { image: Function }) =>
+	z.object({
+		title: z.string(),
+		description: z.string().optional(),
+		publishDate: z.coerce.date().optional(),
+		tags: z.array(z.string()).optional(),
+		image: image(),
+	});
 
 const project = defineCollection({
 	type: "content",
 	schema: projectSchema,
 });
 
-export type ProjectSchema = z.infer<typeof projectSchema>;
+// export type ProjectSchema = z.infer<typeof projectSchema>;
 
 export const collections = {
 	project,
