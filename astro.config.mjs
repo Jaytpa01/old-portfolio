7dc60f22c5ee6154d7e7edc8549edc4d64bbed50
Jaytpa01 <jaytpa01@gmail.com>
2023-10-21 21:15:19 +1100

    chore: install mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jayparry.dev",
-	integrations: [tailwind(), sitemap()],
+  site: "https://jayparry.dev",
+  integrations: [tailwind(), sitemap(), mdx()],
 });
