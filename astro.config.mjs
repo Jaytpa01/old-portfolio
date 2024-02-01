2040f0a5c735f60511b7726dd24d35346f3c1bd6
Jaytpa01 <jaytpa01@gmail.com>
2024-02-01 21:25:51 +1100

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jayparry.dev",
+	site: "https://old.jayparry.dev",
 	integrations: [tailwind(), sitemap()],
 });
